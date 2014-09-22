131aa4b3edcbe570f47ce6a62be47cdd2d639ebc
Joey Goode <jgoode@ccs.neu.edu>
2014-09-21 22:37:10 -0400

    Closes #1.  This is all the stuff mark had before.
    It disregards all the standard unity stuff, so we should make a new issue
    to add instructions about how to get that to the README

---
+++ fortgame/Assets/Scripts/OneDBox.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneDBox : MonoBehaviour {
+
+    public float x;
+    public float velocity;
+    public bool immovable;
+    public float width;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void FixedUpdate () {
+        if (!immovable) {
+            x += velocity * Time.fixedDeltaTime;
+        }
+    }
+    
+    bool overlap (OneDBox box) {
+        if (this.x > box.x) {
+            //overlap right
+            if (this.x < box.x + box.width) {
+                return true;
+            }
+            //overlap enveloped
+            if (this.x + this.width < box.x + box.width) {
+                return true;
+            } 
+            //too far right
+            return false;
+        }
+        //overlap left
+        if (this.x + this.width > box.x) {
+            return true;
+        }
+        //overlap enveloping
+        if (this.x + this.width > box.x + box.width) {
+            return true;
+        }
+        return false;
+    }
+    
+    void separate(OneDBox box) {
+        if (this.immovable && box.immovable) {
+            return;
+        }
+        if (this.x < box.x) {
+            
+        }
+    }
+}
---
+++ fortgame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float walkSpeed = 50.0f;
+    public string walkAxis = "Horizontal";
+    
+    private Vector3 velocity = new Vector3(0.0f,0.0f,0.0f);
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+        //input
+        float walkDir = Input.GetAxis(walkAxis);
+        //physics
+        velocity = new Vector3(walkDir * walkSpeed, 0, 0);
+        //animation
+		if (walkDir < 0) {
+            transform.localScale = new Vector3 (-1, 1, 1);
+		} 
+        else if (walkDir > 0) {
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+	}
+
+	//Fixed Update is called at a fixed timestep
+	void FixedUpdate () {
+		transform.position += velocity * Time.fixedDeltaTime;
+	}
+}
