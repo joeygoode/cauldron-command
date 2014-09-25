7eca3f02341a6cee07dc74b0bd4706c835b50809
Joey Goode <jgoode@ccs.neu.edu>
2014-09-25 16:33:45 -0400

    Add basic entity capable of movement

---
+++ fortgame/Assets/Scripts/OneDBox.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneDBox {
+
+    public float x;
+    public float velocity;
+    public float width;
+
+    public OneDBox (float x, float width, float velocity) {
+        this.x = x;
+        this.velocity = velocity;
+        this.width = width;
+    }
+
+    public void FixedUpdate () {
+        x += velocity * Time.fixedDeltaTime;
+    }
+}
