610357afc5e3fb4561f403c977b26e29db605269
Joey Goode <Gentle.manne.goode@gmail.com>
2014-09-29 22:57:13 -0400

    Add basic collision body for forts

---
+++ fortgame/Assets/Scripts/Fort.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Fort : MonoBehaviour {
+    
+    public int hitpoints = 100;    
+    public float width = 32;
+    public int faction = 0;
+    public GameObject cauldronPrefab;
+
+    public List<Mob> mobs = new List<Mob>();
+    public List<Cauldron> cauldrons = new List<Cauldron>();
+    
+    [HideInInspector]
+    public OneDBox box = new OneDBox(0,32, 0);
+    
+	// Use this for initialization
+	void Start () {
+        box = new OneDBox(transform.position.x - width / 2, width, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    
+    void FixedUpdate () {
+        box.FixedUpdate();
+    }
+}
