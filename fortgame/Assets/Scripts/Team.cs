8414ad24d08648ccca40a613b6f3f675e46347d0
Joey Goode <Gentle.manne.goode@gmail.com>
2014-09-29 23:32:28 -0400

    Add teams encapsulating forts, players, scores

---
+++ fortgame/Assets/Scripts/Team.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Team : MonoBehaviour {
+    //strings for getting the game objects placed in the scene
+    public string FortName;
+    public string PlayerName;
+    public string ScoreName;
+    //GameObjects in the scene
+    private GameObject FortObj;
+    private GameObject PlayerObj;
+    private GameObject ScoreObj;
+    //Fort script objects
+    private Fort Fort;
+    //Character script objects
+    private PlayerController Player;
+    //Text display objects
+    private GUIText Score;
+	// Use this for initialization
+	void Start () {
+        FortObj = GameObject.Find(FortName);
+        PlayerObj = GameObject.Find(PlayerName);
+        ScoreObj = GameObject.Find(ScoreName);
+        
+        Fort = FortObj.GetComponent<Fort>();
+        Player = PlayerObj.GetComponent<PlayerController>();
+        Score = ScoreObj.GetComponent<GUIText>();
+	}
+	
+    // Update is called once per frame
+    void Update () {
+        Score.text = "HP: " + Fort.hitpoints;
+    }
+}
