d5e2378597f06d5a6ebea3b217e5162904ce776b
Joey Goode <jgoode@ccs.neu.edu>
2014-09-30 13:12:28 -0400

    Add basic collisions to OneDBox

--- fortgame/Assets/Scripts/OneDBox.cs
+++ fortgame/Assets/Scripts/OneDBox.cs
@@ -16,4 +16,28 @@ public class OneDBox {
     public void FixedUpdate () {
         x += velocity * Time.fixedDeltaTime;
     }
+
+    public bool overlap (OneDBox box) {
+        if (this.x > box.x) {
+            //overlap right
+            if (this.x < box.x + box.width) {
+                return true;
+            }
+            //overlap enveloped
+            if (this.x + this.width < box.x + box.width) {
+                return true;
+            }
+            //too far right
+            return false;
+        }
+        //overlap left
+        if (this.x + this.width > box.x) {
+            return true;
+        }
+        //overlap enveloping
+        if (this.x + this.width > box.x + box.width) {
+            return true;
+        }
+        return false;
+    }
 }
