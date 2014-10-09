cce3afb0de38ae463fb878addaf10e2c88121f02
BluShine <umbreon27@gmail.com>
2014-10-09 01:40:37 -0700

    Add squishing code to Team
    Added squishing code to team. This is mostly unmodified from the
    previous prototype that had squishing code. Except that now the Team
    handles squishing instead of the Fort.

--- fortgame/Assets/Scripts/Team.cs
+++ fortgame/Assets/Scripts/Team.cs
@@ -33,6 +33,72 @@ public class Team : MonoBehaviour {
         Score.text = "HP: " + fort.hitpoints;
     }
 
+    // Fixed Update is called 
+    void FixedUpdate()
+    {
+        StackMobs();
+
+    }
+
+    private void StackMobs()
+    {
+        //collide mobs with themselves, and
+        // stack them in an animated way based on their position relative to their width/height
+
+        //reset stacking vars
+        foreach (Mob m in mobs)
+        {
+            m.squish = 0;
+            m.y = 0;
+        }
+
+        //convert to array to iterate over
+        Mob[] mobArr = mobs.ToArray();
+        //collide every mob with every mob (except itself)
+        //and only do it if there actually is more than 1 mob
+        if (mobArr.Length > 1)
+        {
+            for (int i = 0; i < mobArr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < mobArr.Length; j++)
+                {
+                    if (mobArr[i].box.overlap(mobArr[j].box))
+                    {
+                        //calculate ratio for squishiness/height boost
+                        bool iThin = true;
+                        float thinner = mobArr[i].width;
+                        if (mobArr[j].width < thinner)
+                        {
+                            thinner = mobArr[j].width;
+                            iThin = false;
+                        }
+                        float overlapRatio = mobArr[i].box.overlapAmount(mobArr[j].box) / thinner;
+                        //thinner mob goes on top
+                        Mob thinMob;
+                        Mob thickMob;
+                        if (iThin)
+                        {
+                            thinMob = mobArr[i];
+                            thickMob = mobArr[j];
+                        }
+                        else
+                        {
+                            thinMob = mobArr[j];
+                            thickMob = mobArr[i];
+                        }
+                        //set squishiness only if we are the "heaviest" thing squishing the mob.
+                        //"heaviest" meaning "thing that's squishing it the most"
+                        if (overlapRatio > mobArr[i].squish)
+                        {
+                            thickMob.squish = overlapRatio * thickMob.squishiness;
+                        }
+                        thinMob.y += overlapRatio * thickMob.height * (1 - thickMob.squishiness);
+                    }
+                }
+            }
+        }
+    }
+
     public void SpawnMobs () {
         if (LastMob == 0)
         {
