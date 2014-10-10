65b8e8200dad0eafb7e2220b61db2c05fa05a957
BluShine <umbreon27@gmail.com>
2014-10-09 20:18:30 -0700

    Added SacrificialAltar class
    Sacrificial altar class for altars. Has a collider box, a function to
    receive resources, and a queue to store upcoming mobs (rather than
    spawning them in a big death stack).

---
+++ fortgame/Assets/Scripts/SacrificialAltar.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SacrificialAltar : MonoBehaviour {
+
+    public float width = 10;
+    public int spawnAmount = 5;
+    public float spawnTime = 0.5f;
+    [HideInInspector]
+    public OneDBox box;
+    public Team team;
+    private float spawnTimer = 0.1f;
+    private Queue<Resource> mobQueue;
+
+	// Use this for initialization
+	void Start () {
+        box = new OneDBox(transform.position.x - (width / 2), width, 0);
+        mobQueue = new Queue<Resource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void FixedUpdate()
+    {
+        if (mobQueue.Count > 0)
+        {
+            spawnTimer -= Time.fixedDeltaTime;
+            if (spawnTimer < 0)
+            {
+                spawnTimer = spawnTime;
+                team.SpawnMob(mobQueue.Dequeue());
+            }
+        }
+    }
+
+    public void recieveResource(Resource resource)
+    {
+        for (int i = 0; i < spawnAmount; i++)
+        {
+            mobQueue.Enqueue(resource);
+        }
+    }
+}
