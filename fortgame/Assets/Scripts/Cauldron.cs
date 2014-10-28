7ab7d8735852a0139183ec967e8c92fbeb488b7e
BluShine <umbreon27@gmail.com>
2014-10-28 05:11:10 -0400

    Issue 9 cauldrons
    Added cauldron class to store cauldron data.

---
+++ fortgame/Assets/Scripts/Cauldron.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Cauldron : MonoBehaviour {
+
+    public float width = 20;
+    int floor = 1;
+    Resource mob;
+    [HideInInspector]
+    OneDBox box;
+
+	// Use this for initialization
+	void Start () {
+        box = new OneDBox(transform.position.x - (width / 2), width, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void Destroy()
+    {
+        Object.Destroy(this.gameObject);
+    }
+}
